2aa9e0cf95c5dc24cbfea0e26db70c455d7ad460
4ukuta <nikita.melnikov95@mail.com>
2018-11-29 23:14:25 +0300

    Fix server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000;
+const clientDir = 'dist';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static files
+app.use(express.static(path.join(__dirname, clientDir)));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, clientDir, 'index.html'));
+});
+
+app.listen(port, () => console.log(`Port server: ${port}`));
